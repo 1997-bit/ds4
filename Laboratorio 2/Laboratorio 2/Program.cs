3ba40f997e12ae859db07199ed7b21ee2ed5c295
Ju3juan <ju3juan@gmail.com>
2025-09-15 19:42:38 -0500

    laboratorio 2 y parte del 3

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Juan";
+            client.LastName = "García";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public ushort Age { get; set; }
+
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ Laboratorio 3/Laboratorio31/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b, OP;
+        Console.WriteLine("Ingrese el primer numero: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        OP = (a + b) * (a - b);
+
+        Console.WriteLine("La suma de {0} + {1} es: {2}", a, b, OP);
+    }
+}
