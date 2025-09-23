0352a53fa7046ae0d3173b4352581fc0349d4db3
Ju3juan <ju3juan@gmail.com>
2025-09-22 19:29:52 -0500

    feat: agregar Laboratorio 5

---
+++ Laboratorio 5/Lab 5-5/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    class Estudiante
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>
+    {
+        new Estudiante { Nombre = "Juan", Edad = 20 },
+        new Estudiante { Nombre = "María", Edad = 22 },
+        new Estudiante { Nombre = "Pedro", Edad = 21 }
+    };
+        foreach (Estudiante estudiante in estudiantes)
+        {
+            Console.WriteLine("Nombre: {estudiante.Nombre}, Edad:" + estudiante.Edad);
+        }
+
+    }
+}
+
+
