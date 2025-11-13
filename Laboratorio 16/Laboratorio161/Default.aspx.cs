1be7605314b4758267b4d5a688cbfc3ee63a531a
Juan García <ju3juan@gmail.com>
2025-11-12 20:05:05 -0600

    Laboratorio 16

---
+++ Laboratorio 16/Laboratorio161/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboratorio161
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMensaje_click(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "Hola, este es tu mensaje.";
+        }
+
+
+    }
+}
