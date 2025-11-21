2bf393c477fd3362bc7743ab063eab2727f56166
Desktop <desktop@local>
2025-11-20 19:59:49 -0600

    laboratorio 19

---
+++ Laboratorio 19/Laboratorio192/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Laboratorio193.Controllers
+{
+    public class ValuesController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<ActionResult> GetValueById(int id = 2)
+        {
+            try
+            {
+                // Cambia el puerto por el que usa tu API del Laboratorio 19-1
+                string apiUrl = $"https://localhost:44360/api/values/{id}";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    ViewBag.Value = responseBody;
+                    ViewBag.Message = $"Dato con ID {id} obtenido correctamente";
+                    ViewBag.Id = id;
+                }
+                else
+                {
+                    ViewBag.Message = "Error al obtener dato: " + response.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Error: " + ex.Message;
+            }
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int id)
+        {
+            return RedirectToAction("GetValueById", new { id = id });
+        }
+    }
+}
---
+++ Laboratorio 19/Laboratorio193/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Laboratorio192.Controllers
+{
+    public class ValuesController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<ActionResult> GetAllValues()
+        {
+            try
+            {
+                // Cambia el puerto por el que usa tu API del Laboratorio 19-1
+                string apiUrl = "https://localhost:44360/api/values";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    ViewBag.Values = responseBody;
+                    ViewBag.Message = "Datos obtenidos correctamente";
+                }
+                else
+                {
+                    ViewBag.Message = "Error al obtener datos: " + response.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Error: " + ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
